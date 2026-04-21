31c52b581ca0cc5c03545533c79b1f5b92d5093b
Ericson Olaya Castillo <davidolayacs95@gmail.com>
2026-04-21 16:58:05 -0600

    agregando imagenes para el carrusel

--- slide.js
+++ slide.js
@@ -1,8 +1,21 @@
-const slides = document.querySelectorAll('.slide');
-let indiceActual = 0;
-function cambiarImagen() {
-    slides[indiceActual].classList.remove('active');
-    indiceActual = (indiceActual + 1) % slides.length;
-    slides[indiceActual].classList.add('active');
-}
-setInterval(cambiarImagen, 3000);
+document.addEventListener('DOMContentLoaded', () => {
+
+const carruseles = document.querySelectorAll('.carrusel');
+
+carruseles.forEach((carrusel) => {
+    let slides = carrusel.querySelectorAll('.slide');
+    let index = 0;
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+    }, 3000);
+});
+});
+        
+
+
+
+
+
